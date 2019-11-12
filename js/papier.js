0c00f8e50f0aa239b9706afc3df38ac5f6291240
misza <zielinskanoemi5@gmail.com>
2019-11-12 13:39:41 +0100

    Add name submit

--- js/papier.js
+++ js/papier.js
@@ -1,11 +1,20 @@
-var kamien = "kamien";
-var nozyce = "nozyce";
-var papier = "papier";
+let kamien = "kamien";
+let nozyce = "nozyce";
+let papier = "papier";
 
-var pickkamien = document.getElementById(kamien);
-var pickpapier = document.getElementById(papier);
-var picknozyce = document.getElementById(nozyce);
+let pickkamien = document.getElementById(kamien);
+let pickpapier = document.getElementById(papier);
+let picknozyce = document.getElementById(nozyce);
+let playerName;
 
+document.querySelector("#name button")
+    .addEventListener('click', function(){
+        let inputEl = document.querySelector("#name input");
+        playerName = inputEl.value;
+        let game = document.querySelector(".container-button").style.display = "flex";
+        let name = document.querySelector("#name").style.display = "none";
+
+    });
 
 pickkamien.addEventListener('click', function () {
     game(kamien)
@@ -21,7 +30,7 @@ picknozyce.addEventListener('click', function () {
 
 
 function compPick() {
-    var rand = Math.floor(Math.random() * 3 + 1);
+    let rand = Math.floor(Math.random() * 3 + 1);
     if (rand == 1) {
         return kamien;
     } else if (rand == 2) {
@@ -33,13 +42,13 @@ function compPick() {
 
 
 function game(pick) {
-    var compPick1 = compPick();
+    let compPick1 = compPick();
     if (pick === compPick1) {
         alert("Remis!\nKomputer wybrał: " + compPick1)
-    } else if (pick === papier && compPick1 === "kamien" ||
-        pick === kamien && compPick1 === "nozyce" ||
-        pick === nozyce && compPick1 === "papier") {
-        alert("Gracz wygrał!\nKomputer wybrał: " + compPick1)
+    } else if (pick === papier && compPick1 === kamien ||
+        pick === kamien && compPick1 === nozyce ||
+        pick === nozyce && compPick1 === papier) {
+        alert(playerName + " wygrał!\nKomputer wybrał: " + compPick1)
     } else {
         alert("Komputer wygrał!\nKomputer wybrał: " + compPick1)
     }
